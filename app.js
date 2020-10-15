9d43c46aff56a7b2ec7cb7a30a95b77b99ca9a6b
Seid Mohamed <seid783@gmail.com>
2020-10-14 21:53:36 -0700

    css change

---
+++ app.js
@@ -0,0 +1,11 @@
+var username = prompt('Whats your Last name');
+
+alert('Greetings' + userName);
+
+
+if(userName == 'Seid'){
+    document.write('Greetings Admin' + userName)
+} 
+else {
+    document.write('Greetings' + userName)
+}
